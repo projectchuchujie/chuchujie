2706d4042930c079926d986279dec5d5291d9718
18674048791 <1336903108@qq.com>
2016-10-31 21:32:02 +0800

    'ssd'

--- components/mine/mine.js
+++ components/mine/mine.js
@@ -3,7 +3,7 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider
 	.when('/mine', {
 		templateUrl: 'components/mine/mine.html',
-//		controller:'mineCtrl',
+		controller:'mineCtrl',
 		css:'components/mine/mine.css'
 	})
 }]);
@@ -14,8 +14,27 @@ app.config(['$routeProvider', function($routeProvider){
 //	}
 //}]);
 //
-//app.controller("mineCtrl",["$scope","mineService",function($scope,mineService){
-//	mineService.get().success(function(res){
-//		$scope.minearr=res.data;
-//	})
-//}])
+app.controller("mineCtrl",["$scope",function($scope){
+	var tel = document.querySelector('#tel');
+	tel.onfocus=function(){
+		$(this).attr('placeholder',"");
+	}
+	tel.onblur=function(){
+		$(this).attr('placeholder',"手机号");
+	}
+	
+	var password = document.querySelector('#password');
+	password.onfocus=function(){
+		$(this).attr('placeholder',"");
+	}
+	password.onblur=function(){
+		$(this).attr('placeholder',"密码");
+	}
+	
+	var logins = document.querySelector('#logins');
+		logins.onclick=function(){
+			$('#mine-wrap1').hide();
+			$('#mine-wrap2').show();
+		}
+}])
+
