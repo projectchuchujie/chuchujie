1edf3d99de6ae310ada34953f03232055d63053d
ss <675513653@qq.com>
2016-11-04 19:34:16 +0800

    '搜索页面'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app=angular.module('ccj',['ngRoute','angularCSS','homeModule','mineModule','nineModule','classifyModule','searchModule','carModule','me-lazyload','ngAnimate'])
+var app=angular.module('ccj',['ngRoute','angularCSS','homeModule','mineModule','nineModule','classifyModule','searchModule','carModule','goodsModule','me-lazyload','ngAnimate'])
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
 	.otherwise({
--- components/search/search.js
+++ components/search/search.js
@@ -8,14 +8,45 @@ app.config(['$routeProvider', function($routeProvider){
 	})
 }]);
 
-//app.service("searchService",["$http",function($http){
-//	this.get=function(){
-//		return $http.get('json/homeman.json');
-//	}
-//}]);
-//
-app.controller("searchCtrl",["$scope",function($scope){
-		$scope.tiaozhuan=function(){
-			window.location.href="http://127.0.0.1:8020/chuchujie/index.html#/home";
-		}
+app.service("searchService",["$http",function($http){
+	this.get=function(){
+		return $http.get('json/classifywomen.json');
+	}
+}]);
+
+
+app.factory('myService', function() {
+	 var savedData = {}
+	 function set(data) {
+	   savedData = data;
+	 }
+	 function get() {
+	  return savedData;
+	 }
+	
+	 return {
+	  set: set,
+	  get: get
+	 }
+
+})
+
+app.controller("searchCtrl",["$scope","searchService",'myService',function($scope,searchService,myService){
+	searchService.get().success(function(res){
+		$scope.searcharr=res.data;
+	})
+	$scope.tiaozhuan=function(){
+		window.location.href="http://127.0.0.1:8020/chuchujie/index.html#/home";
+	}
+	$scope.flag=true;
+	$scope.search=function(){
+		$scope.flag=false;
+		$scope.select=$('#search-ip').val();
+	}
+	$scope.clickItem=function(obj){
+		myService.set(obj);
+	}
 }])
+
+
+ 
